9f9be3d4894504a6ef433046de86cb56d98e4593
jaymzee <jaymzee@users.noreply.github.com>
2020-10-05 10:40:26 -0400

    learn/vars1.rs

---
+++ learn/vars1.rs
@@ -0,0 +1,18 @@
+const Z: i32 = 42;
+
+fn foo(name: &str) {
+    println!("hello {}", name);
+}
+
+fn main() {
+    let x: i8 = 100;
+    let y: u8 = 5;
+    let name = "george";
+    let mut s = String::from("joe");
+
+    foo(name);
+    foo(&s);
+    s.push_str(" shmo");
+    foo(&s);
+    println!("x = {}", Z + i32::from(x) + i32::from(y))
+}
