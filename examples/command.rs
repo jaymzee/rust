9ddcd1e43c4f1f5b2872160805426e74faf557cc
jaymzee <jaymzee@users.noreply.github.com>
2021-03-17 10:53:21 -0400

    command example

---
+++ examples/command.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+
+fn main() {
+    /*
+    let child = Command::new("/bin/cat")
+        .arg("colors")
+        .current_dir("/home/james/Documents")
+        .stdout(Stdio::piped())
+        .status()
+        .expect("command failed to start");
+    */
+
+    let output = Command::new("git")
+        .arg("log")
+        .arg("--oneline")
+        .output()
+        .expect("can't run git!");
+
+    println!("status: {}", output.status);
+    println!("stdout: \n{}", String::from_utf8_lossy(&output.stdout));
+}
