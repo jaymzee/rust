f1a9f09d09ec9dda90c771c03b74b855e85ed2be
jaymzee <jaymzee@users.noreply.github.com>
2021-05-29 20:32:42 -0400

    examples/sdl2 - cleanup

--- examples/sdl2/src/main.rs
+++ examples/sdl2/src/main.rs
@@ -1,11 +1,10 @@
-extern crate sdl2;
-
-use sdl2::gfx::primitives::DrawRenderer;
-//use sdl2::rect::{Rect, Point};
-use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use std::thread;
 use std::time::Duration;
+use sdl2::event::Event;
+use sdl2::gfx::primitives::DrawRenderer;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+//use sdl2::rect::{Rect, Point};
 
 pub fn main() {
     let sdl_context = sdl2::init()
@@ -41,6 +40,6 @@ pub fn main() {
         // The rest of the game loop goes here...
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
