5ff4a704ce95f1630b4e0220590d0f8a8b609796
jaymzee <jaymzee@users.noreply.github.com>
2021-07-05 11:17:42 -0400

    minigrep

--- examples/map.rs
+++ examples/map.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let x: Vec<i32> = (1 .. 10).collect();
-    let y: Vec<i32> = x.iter().map(|&x| x * x).collect();
+    let a = Some(3);
+    let b: Result<_, String> = Ok(4);
+    let c: Vec<i32> = (1..10).collect();
+    let x = a.map(|x| x * x);
+    let y = b.map(|x| x * x);
+    let z: Vec<_> = c.iter().map(|x| x * x).collect();
 
+    println!("{:?}", x);
     println!("{:?}", y);
+    println!("{:?}", z);
 }
