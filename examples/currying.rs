11d2eb99e077a6854bf152b6050cebac23ada4bb
jaymzee <jaymzee@users.noreply.github.com>
2021-07-09 05:28:43 -0400

    updated currying example

--- examples/currying.rs
+++ examples/currying.rs
@@ -3,4 +3,7 @@ fn add(a: i32, b: i32) -> i32 { a + b }
 fn main() {
     let add5 = |x| add(5, x);
     println!("{}", add5(2));
+
+    let mul = |x| move |y| x * y;
+    println!("{}", mul(2)(3));
 }
