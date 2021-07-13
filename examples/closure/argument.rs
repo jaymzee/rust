c24ade4a5b4280a2f6ae4102bc1cbcaffbdef264
jaymzee <jaymzee@users.noreply.github.com>
2021-07-13 13:24:43 -0400

    closure FnMut example

--- examples/closure/argument.rs
+++ examples/closure/argument.rs
@@ -4,9 +4,24 @@ fn main() {
     }
 
     use_fn_or_closure(f);
-    use_fn_or_closure(|x| 3 * x);
+
+    let mut y = 5;
+
+    use_closure2(|x| {
+        y += 1;
+        x * y
+    });
+
+    println!("{}", y);
 }
 
+// only works if closure does not capture
 fn use_fn_or_closure(f: fn(i32) -> i32) {
     println!("{}", f(4));
+}
+
+fn use_closure2(mut f: impl FnMut(i32) -> i32) {
+    println!("{}", f(3));
+    println!("{}", f(4));
+    println!("{}", f(5));
 }
