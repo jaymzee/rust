f70dbebfc430cbe14006792fa28c30a276fec5d6
jaymzee <jaymzee@users.noreply.github.com>
2021-09-18 01:21:39 -0400

    math/la

--- math/la/src/serial.rs
+++ math/la/src/serial.rs
@@ -1,1 +1,11 @@
-use nalgebra
+use nalgebra::DMatrix;
+
+fn main() -> Result<(), std::io::Error> {
+    let row_slice: Vec<i32> = (1..=5000).collect();
+    let matrix = DMatrix::from_row_slice(50, 100, &row_slice);
+
+    let ser_matrix = serde_json::to_string(&matrix);
+    let des_matrix: DMatrix<i32> = serde_json::from_str(&ser_matrix)?;
+
+    assert!(des_matrix == matrix);
+}
