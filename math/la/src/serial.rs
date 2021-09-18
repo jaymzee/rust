ac40f00a8b6737915d3c6fa33f5ea600157ffc06
jaymzee <jaymzee@users.noreply.github.com>
2021-09-18 01:59:06 -0400

    math/la nalgebra serialization example

--- math/la/src/serial.rs
+++ math/la/src/serial.rs
@@ -1,11 +1,13 @@
 use nalgebra::DMatrix;
 
-fn main() -> Result<(), std::io::Error> {
+pub fn example()-> Result<(), std::io::Error> {
     let row_slice: Vec<i32> = (1..=5000).collect();
     let matrix = DMatrix::from_row_slice(50, 100, &row_slice);
 
-    let ser_matrix = serde_json::to_string(&matrix);
+    let ser_matrix = serde_json::to_string(&matrix)?;
     let des_matrix: DMatrix<i32> = serde_json::from_str(&ser_matrix)?;
 
     assert!(des_matrix == matrix);
+
+    Ok(())
 }
